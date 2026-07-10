7e3e69db49cc02fa614e1b36b1d012c9006d0373
666arevi <arevin6019@gmail.com>
2026-07-10 19:16:26 +0300

    Сделал 2 функцию, которое обрабатывает спсики поступающих. Нужно исправить баг с нумерацией и правильно смержить все клетки

--- splitter/main.py
+++ splitter/main.py
@@ -1,17 +1,21 @@
-from openpyxl import *
+import openpyxl
 import re
 from tools import *
 from pathlib import Path
+import json
 
 BASE_DIR = Path(__file__).parent.resolve()
 READY_DIR = BASE_DIR / "ready"
-path_from = r"C:\Users\arevi\Yandex.Disk\Computer STO_ORMIK\Documents\Програмирование\python\projects\priemka\splitter\Все_заявления_(бак_спец_БВО)_09-07_11-07-10.xlsx"
+path_gosuslugi= r"C:\Users\arevi\Yandex.Disk\Computer STO_ORMIK\Documents\Програмирование\python\projects\priemka\splitter\Все_заявления_(бак_спец_БВО)_09-07_11-07-10.xlsx"
+path_admission = r"C:\Users\arevi\Yandex.Disk\Computer STO_ORMIK\Documents\Програмирование\python\projects\priemka\splitter\Списки поступающих бакалавриат.xlsx"
 
-wd = load_workbook(path_from)
-ws = wd["Sheet1"]
+
+
 
 def gosuslugi_splitter():
-    
+    wd = openpyxl.load_workbook(path_gosuslugi)
+    ws = wd["Sheet1"]
+
     data: dict = {}
     count: int = 0
     current_row: int = 1
@@ -28,6 +32,8 @@ def gosuslugi_splitter():
             sports = list(set(sports))
             print(sports)
             amount_split = len(sports) - 1
+            # сохраняем виды, которое нужно добавить, а также индентификатор
+            data[row[1].value] = sports
             # первую изменяем сразу
             row[34].value = sports[0]
 
@@ -39,17 +45,77 @@ def gosuslugi_splitter():
                 insert_data_to_row(ws, row, id)
                 ws[id][34].value = sport
                 id += 1
+
+        elif row[34].value and row[1].value not in data:
+            data[row[1].value] = row[34].value
             
         current_row += 1
-    print(row[34].row)
 
-    # заполняем строки данными
-    # for id, value in data.items():
-    #     for sport in sports[1:]:
-    #         insert_data_to_row(ws, value, id)
-    #         ws[id][34].value = sport
-    #         id += 1
+    wd.save(BASE_DIR / "temp" / "gosuslugi.xlsx")
+    return data
+
+# res = gosuslugi_splitter()
+# with open(BASE_DIR / "json.json", mode="w", encoding="utf-8") as f:
+#     print(res)
+#     json.dump(res, f, ensure_ascii=False, indent=4)
+
+with open(BASE_DIR / "json.json", mode="r", encoding="utf-8") as f:
+    data = json.load(f)
+
+def admission_lists_splitter_add():
+    wd_admission = openpyxl.load_workbook(path_admission)
+    # wd_gosuslugi = openpyxl.load_workbook(BASE_DIR / "temp" / "gosuslugi.xlsx")
+    ws_adm = wd_admission.active
+    print(ws_adm.max_row)
+
+    pattern: str = "Спортивная подготовка по виду спорта. Тренерско-преподавательская деятельность в образовании"
+    current_row: int = 850
+
+    stopper = 0
+    while current_row <= ws_adm.max_row:
+        row = ws_adm[current_row]
+
+        if row[8].value == pattern:
+            sports = data.get(row[1].value)
+            print(f"row: {current_row}, sports: {sports}, max_row: {ws_adm.max_row}")
+            if isinstance(sports, str):
+                # добавляем вид спорта и фомратируем
+                row[8].value += "; " + sports
+                ws_adm.row_dimensions[current_row].height = 160
+
+            elif isinstance(sports, list):
+                # изменяем текущию строку
+                row[8].value += "; " + sports[0]
+                # получаем количество строк которое нужно вставить
+                amount_split = len(sports) - 1
+                if amount_split <= 0:
+                    continue
+                id = current_row + 1
+
+                safe_insert_rows(ws_adm, current_row, amount_split)         
+                # заполняем новые строки
+                for sport in sports[1:]:
+                    # вставляем строки
+                    insert_data_to_row(ws_adm, row, id, styles=True)
+                    ws_adm[id][8].value = pattern+  "; " + sport
+                    id += 1
+        
+                current_row += amount_split
+
+                
+        stopper += 1
+        current_row += 1
+
+    # форматируем все обьединенные ячейки
+    formatted_merged_cells(ws_adm)
+    height_formatted(ws_adm)
+    wd_admission.save(BASE_DIR / "ready" / "execel.xlsx")
 
 
-gosuslugi_splitter()
-wd.save(READY_DIR / "test_new.xlsx")
+admission_lists_splitter_add()
+
+
+
+
+
+
--- splitter/tools.py
+++ splitter/tools.py
@@ -1,13 +1,74 @@
+import openpyxl
+from openpyxl.worksheet.worksheet import Worksheet
 
+# Настройка стилей
+thin_side = openpyxl.styles.Side(border_style="thin", color="000000")
+my_border = openpyxl.styles.Border(left=thin_side, right=thin_side, top=thin_side, bottom=thin_side)
+my_font = openpyxl.styles.Font(name="Times New Roman", size=10)
+my_alignment = openpyxl.styles.Alignment(wrap_text=True, vertical="center", horizontal="center")
 
-def insert_data_to_row(workshhet, row_out: list | tuple, row_id):
+pattern: str = "Спортивная подготовка по виду спорта. Тренерско-преподавательская деятельность в образовании"
+
+def insert_data_to_row(workshhet, row_out: list | tuple, row_id, styles: bool = False):
     """Функция вставляет строки в определенную строку
 
     Args:
         workshhet (WorkSheet): рабочий лист WoorkBook
         row_out (list | tuple): список ячеек, которые нужно вставить 
         row_id (int): id строки, куда надо вставить ячейки
+        styles (bool): флаг, указывающий, нужно ли применять стили
     """
+    
     row_in = workshhet[row_id]
     for cell_in, cell_out in zip(row_in, row_out):
         cell_in.value = cell_out.value
+        if styles:
+            cell_in.border = my_border
+            cell_in.font = my_font
+            cell_in.alignment = my_alignment
+            
+            # мержим некоторые колонки 
+            workshhet.merge_cells(f"O{row_id}:P{row_id}")
+            workshhet.merge_cells(f"R{row_id}:T{row_id}")
+            workshhet.merge_cells(f"U{row_id}:V{row_id}")
+
+            if cell_out.value in ["Заочная", "Очная"]:
+                cell_in.alignment = openpyxl.styles.Alignment(wrap_text=True, vertical="center")
+            elif cell_out.value and cell_out.value.startswith(pattern):
+                cell_in.alignment = openpyxl.styles.Alignment(wrap_text=True, vertical="center")
+            
+            # настраиваем высоту
+            workshhet.row_dimensions[row_id].height = 160
+
+
+def safe_insert_rows(ws, start_row, amount):
+    """
+    Безопасно вставляет строки, сдвигая координаты всех объединенных ячеек ниже.
+    """
+    # 1. Сначала сдвигаем границы всех объединенных диапазонов, которые находятся ниже
+    for merged_range in list(ws.merged_cells.ranges):
+        # Если объединенная ячейка начинается ниже или на уровне вставки
+        if merged_range.min_row >= start_row:
+            merged_range.shift(row_shift=amount, col_shift=0)
+        # Если вставка происходит ВНУТРИ объединенного диапазона (расширяем его)
+        elif merged_range.max_row >= start_row:
+            merged_range.max_row += amount
+        
+
+    # 2. Теперь выполняем стандартную вставку строк
+    ws.insert_rows(idx=start_row + 1, amount=amount)
+    
+    
+
+def formatted_merged_cells(ws):
+    for merged_range in ws.merged_cells.ranges:
+        columns_count = merged_range.max_col - merged_range.min_col + 1
+
+        if columns_count >= 21:
+            row_idx = merged_range.min_row
+            ws.row_dimensions[row_idx].height = 13
+
+def height_formatted(ws: Worksheet):
+    for row_idx, row in enumerate(ws.iter_rows(), 1):
+        if row[8].value and row[8].value.startswith(pattern):
+            ws.row_dimensions[row_idx].height = 160
